d5290e3b1fa7a890f3458ce63c4b73880242b7c3
Cole Hlava <cole.hlava@gmail.com>
2020-05-03 16:51:25 -0700

    Got USB working with sending increasing value to PSoC as %07d format.

--- version1_flask/usbcom.c
+++ version1_flask/usbcom.c
@@ -10,17 +10,21 @@
 #include <libusb.h>
 #include <wiringPiI2C.h>
 
-#define MESSAGE_SIZE 8
-#define INPOINT      2 
+#define MESSAGE_SIZE      8
+#define INPOINT           1 
+#define VENDOR_ID    0x04B4
+#define PRODUCT_ID   0x8051
 
 
 // main
 int main (int argc, char * argv[]) {
   
   // USB variables
-  char message[MESSAGE_SIZE];
   int usb_return_val;
   int sent_bytes;
+  char message[MESSAGE_SIZE] = {"abcdefg\0"};
+  /*for (int i = 0; i < MESSAGE_SIZE; i++)
+    message[i] = 0;*/
 
   // Configure USB
   libusb_init(NULL);            // Initialize the LIBUSB library
@@ -53,35 +57,41 @@ int main (int argc, char * argv[]) {
   }
 
   // Open data files
-  FILE* button_file = fopen("button_status.txt", "r");
-  FILE* spead_file = fopen("speed.txt", "r");
+  //FILE* button_file = fopen("button_status.txt", "r");
+  //FILE* spead_file = fopen("speed.txt", "r");
 
   // Ensure files exist
-  if (button_file == NULL || speed_file == NULL) {
-    printf("Failed to open data files.\n")
+  /*if (button_file == NULL || speed_file == NULL) {
+    printf("Failed to open data files.\n");
     return -1;
-  }
+  }*/
+
+
+  uint8_t send_val = 0;
 
   // Main loop
   while (1) {
       // Format message to be sent over USB
       // TODO ^
-      //
-      
+      // 
+      sprintf(message, "%07d", send_val++);
 
       // Perform OUT transfer (from host to device).
-      usb_return_val = libusb_bulk_transfer(device, INPOINT, message, TRANSFER_SIZE, &sent_bytes, 0);
+      usb_return_val = libusb_bulk_transfer(device, INPOINT, message, MESSAGE_SIZE, &sent_bytes, 0);
 
       // Throw error if OUT transfer failed
       if (usb_return_val != 0) {
           perror("OUT transfer failed\n");
           return -1;
       }
+
+      // Delay
+      for (int i = 0; i < 100000000; i++);
   }
 
   // Cleanup
-  fclose(button_file);
-  fclose(speed_file);
+  //fclose(button_file);
+  //fclose(speed_file);
   libusb_close(device);
   return 0;
 }
